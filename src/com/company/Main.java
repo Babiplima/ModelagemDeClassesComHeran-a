b744432755c5999f9420f8e5e6c3b09d37648bc9
Bárbara Ann Lima <babiplima@gmail.com>
2021-09-23 19:44:27 -0300

    Segundo commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Instanciando objeto Fornecedor
+
+        Fornecedor fornecedor = new Fornecedor("Bárbara","Canitar","21991502997", 1000, 500);
+        System.out.println(fornecedor.obterSaldo());
+        fornecedor.setValorDivida(500);
+        System.out.println(fornecedor.obterSaldo());
+        fornecedor.setValorCredito(1000);
+        System.out.println("O valor do saldo do fornecedor é R$ "+ fornecedor.obterSaldo());
+
+        //Instanciando objeto Colaborador
+
+        Colaborador colaborador = new Colaborador("Matheus", "Canitar","219999990",036,2000, 150);
+
+        //Instanciando objeto Administrador
+
+        Administrador administrador = new Administrador("Rafael", "Canitar","2188877666", 456,10000,300,200);
+
+
     }
 }
